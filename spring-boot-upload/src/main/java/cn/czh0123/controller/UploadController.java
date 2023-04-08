a52814be9fe8aa81dcef141c08c3257a8c4fe173
YunquanCheung <1489996991@qq.com>
2023-04-08 14:33:11 -0400

    fix: return image/images to frontend

--- spring-boot-upload/src/main/java/cn/czh0123/controller/UploadController.java
+++ spring-boot-upload/src/main/java/cn/czh0123/controller/UploadController.java
@@ -2,25 +2,36 @@ package cn.czh0123.controller;
 
 import org.springframework.boot.system.ApplicationHome;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.UUID;
 import java.io.BufferedReader;
+import java.nio.file.Files;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+
 import java.io.InputStreamReader;
 
 @RestController
 @CrossOrigin
 public class UploadController {
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> upload(@RequestParam("image") MultipartFile file) throws IOException {
+    // 单张
+    @PostMapping(value = "/upload", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> upload(@RequestParam("image") MultipartFile file) throws IOException {
         //file校验
         if (file.isEmpty()) {
-//            return "图片上传失败";
-            return ResponseEntity.badRequest().body("No file chosen");
+            return ResponseEntity.badRequest().body(null);
         }
 
         //file重命名 (a: 1.png   b:1.png)
@@ -44,24 +55,102 @@ public class UploadController {
         // String command = "python3 " + pre + "/src/main/java/cn/czh0123/controller/pythonFile/test1.py";
         String command = "/opt/anaconda3/bin/python " + pre + "/src/main/java/cn/czh0123/controller/pythonFile/GradCAM.py";
         System.out.println(command);
-        // String command = "pwd";
-        
+
         Process process = Runtime.getRuntime().exec(command);
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
+        ArrayList<String> arrayString = new ArrayList<>();
         while ((line = reader.readLine()) != null) {
+            arrayString.add(line);
             System.out.println(line);
         }
 
         System.out.println("=======================");
+        // String classification = arrayString.get(3);
         
-        return ResponseEntity.ok(originalFilename);
+
+        // 返回处理后的图片 单张
+        // pre output ====== /Users/ndsjr/Documents/GitHub/MengProject/spring-boot-upload
+        String filePath = pre + "/src/main/resources/static/images/test1.jpeg";
+
+        File newFile = new File(filePath);
+        if (!newFile.exists()) {
+            throw new FileNotFoundException("Image not found");
+        }
+        byte[] imageBytes = Files.readAllBytes(newFile.toPath());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(imageBytes.length);
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
     }
 
-//    @GetMapping
-//    public String upload(MultipartFile file) {
-//
-//
-//        return "";
-//    }
+
+    // // 多张
+    // @PostMapping(value = "/upload", produces = MediaType.IMAGE_JPEG_VALUE)
+    // public ResponseEntity<ArrayList<byte[]>> upload_s(@RequestParam("image") MultipartFile file) throws IOException {
+    //     // file校验
+    //     if (file.isEmpty()) {
+    //         return ResponseEntity.badRequest().body(null);
+    //     }
+
+    //     // file重命名 (a: 1.png b:1.png)
+    //     String originalFilename = file.getOriginalFilename(); // 原来的图片名
+
+    //     // 上传图片
+    //     ApplicationHome applicationHome = new ApplicationHome(this.getClass());
+    //     String pre = applicationHome.getDir().getParentFile().getParentFile().getAbsolutePath();
+    //     System.out.println(pre);
+
+    //     System.out.println(originalFilename);
+    //     String path = pre + "/src/main/resources/static/images/test1.jpeg";
+    //     try {
+    //         file.transferTo(new File(path));
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+
+    //     // 执行python文件
+    //     // String command = "python3 " + pre +
+    //     // "/src/main/java/cn/czh0123/controller/pythonFile/test1.py";
+    //     String command = "/opt/anaconda3/bin/python " + pre
+    //             + "/src/main/java/cn/czh0123/controller/pythonFile/GradCAM.py";
+    //     System.out.println(command);
+
+    //     Process process = Runtime.getRuntime().exec(command);
+    //     BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    //     String line;
+    //     ArrayList<String> arrayString = new ArrayList<>();
+    //     while ((line = reader.readLine()) != null) {
+    //         arrayString.add(line);
+    //         System.out.println(line);
+    //     }
+
+    //     System.out.println("=======================");
+    //     // String classification = arrayString.get(3);
+
+    //     // 返回处理后的图片 多张
+    //     String filePath1 = pre + "/src/main/resources/static/images/test1.jpeg";
+    //     String filePath2 = pre + "/src/main/resources/static/images/test1.jpeg";
+
+    //     File newFile1 = new File(filePath1);
+    //     File newFile2 = new File(filePath2);
+    //     if (!newFile1.exists()) {
+    //         throw new FileNotFoundException("Image not found");
+    //     }
+    //     if (!newFile2.exists()) {
+    //         throw new FileNotFoundException("Image not found");
+    //     }
+
+    //     byte[] imageBytes1 = Files.readAllBytes(newFile1.toPath());
+    //     byte[] imageBytes2 = Files.readAllBytes(newFile2.toPath());
+
+    //     ArrayList<byte[]> imageBytes = new ArrayList<>();
+    //     imageBytes.add(imageBytes1);
+    //     imageBytes.add(imageBytes2);
+
+    //     HttpHeaders headers = new HttpHeaders();
+    //     headers.setContentType(MediaType.IMAGE_JPEG);
+    //     headers.setContentLength(imageBytes.size());
+    //     return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+    // }
 }
