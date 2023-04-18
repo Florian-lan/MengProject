8a5637e208ba85dfb180444ec530117410e1869c
Florian Lan <florianlan@FloriandeMacBook-Pro.local>
2023-04-18 17:58:36 -0400

    add: handsome pic of quan

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,22 +3,25 @@ import { Carousel } from 'antd';
 import "./style.scss";
 
 const Card = () => {
-    return(
+    return (
         <>
             <Carousel effect="fade" slide='false' fade='false'>
                 <div>
-                <h3 className="carousel-content">1</h3>
+                    <h3 className="carousel-content">
+                        <img src="https://github.com/Florian-lan/MengProject/blob/main/public/images/quan2.JPG" alt="" />
+                    </h3>
+
 
                 </div>
                 <div>
-                <h3 className="carousel-content">1</h3>
+                    <h3 className="carousel-content">1</h3>
 
                 </div>
                 <div>
-                <h3 className="carousel-content">1</h3>
+                    <h3 className="carousel-content">1</h3>
                 </div>
                 <div>
-                <h3 className="carousel-content">1</h3>
+                    <h3 className="carousel-content">1</h3>
                 </div>
             </Carousel>
         </>
